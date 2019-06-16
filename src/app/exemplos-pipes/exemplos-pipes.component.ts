c97677468f908fc34058fc53a16aa8049003dda2
Roneison Machado <roneison.machado@gmail.com>
2019-06-15 22:23:51 -0300

    Pipes (usando pipes, parametros e pipes aninhados)

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  produto : any = {
+    descricao: 'PRODUTO DE TESTE',
+    validade: new Date(2019, 12, 31),
+    custo: 14.99,
+    venda: 25.5
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
